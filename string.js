405d25d85061a936f15d45e3a5c1868093001849
Priyanka Sharma <108454203+PRIYANKAKUMARI9@users.noreply.github.com>
2024-01-07 18:36:04 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,11 @@
+console.log("hello ");
+
+let lastName="anurag";
+
+//object
+
+let firstName= new String("jiya");
+
+let message='this is your name';
+let words=message.split(' ');
+console.log (words);
